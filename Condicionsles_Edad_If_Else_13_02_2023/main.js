544f6141f07975b4753c716b6a116f977a639962
JaimeAndresMenesesAparicio <jaime.meneses89@gmail.com>
2023-02-15 19:01:33 -0500

    Ejercicio_1

---
+++ Condicionsles_Edad_If_Else_13_02_2023/main.js
@@ -0,0 +1,7 @@
+let edad = Number(prompt("Ingrese su edad:"));
+if(edad>=18){
+    console.log("Es mayor de edad");
+}
+else{
+    console.error("La edad ingresada no cumple con los parámetros")
+;}
